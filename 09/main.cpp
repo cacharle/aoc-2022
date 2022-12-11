4d6f3088796b73dc688eec0bd15dcad9f1797ecb
Charles Cabergs <me@cacharle.xyz>
2022-12-11 10:53:06 +0100

    Added Day 9 Part 2

--- 09/main.cpp
+++ 09/main.cpp
@@ -8,11 +8,45 @@
 
 struct Position
 {
-    int x;
     int y;
+    int x;
 
     bool operator==(const Position &other) const { return x == other.x && y == other.y; }
 };
+
+auto rope_step(std::vector<Position> &rope,
+               int head_mod_x,
+               int head_mod_y,
+               std::vector<Position> &visited)
+{
+    rope.front().x += head_mod_x;
+    rope.front().y += head_mod_y;
+    for (size_t i = 1; i < rope.size(); i++)
+    {
+        auto next_knot = rope[i - 1];
+        auto mod_x = next_knot.x < rope[i].x ? 1 : -1;
+        auto mod_y = next_knot.y < rope[i].y ? 1 : -1;
+        auto dist_x = abs(next_knot.x - rope[i].x);
+        auto dist_y = abs(next_knot.y - rope[i].y);
+        if (dist_x >= 2 && dist_y >= 2)  // 2 cell away in diagonal in both direction
+        {
+            rope[i].x = next_knot.x + mod_x;
+            rope[i].y = next_knot.y + mod_y;
+        }
+        else if (dist_x >= 2)  // 2 cell away in x (maybe 1 cell diagonal)
+        {
+            rope[i].x = next_knot.x + mod_x;
+            rope[i].y = next_knot.y;
+        }
+        else if (dist_y >= 2)  // 2 cell away in y (maybe 1 cell diagonal)
+        {
+            rope[i].y = next_knot.y + mod_y;
+            rope[i].x = next_knot.x;
+        }
+    }
+    if (std::ranges::find(visited, rope.back()) == visited.end())
+        visited.push_back(rope.back());
+}
 
 auto main() -> int
 {
@@ -20,14 +54,14 @@ auto main() -> int
     file.open("09/input.txt");
     assert(file.is_open());
 
-    auto visited = std::vector<Position>();
-    auto head = Position{0, 0};
-    auto tail = Position{0, 0};
+    auto visited1 = std::vector<Position>();
+    auto visited2 = std::vector<Position>();
+    auto rope1 = std::vector<Position>(2, Position{0, 0});
+    auto rope2 = std::vector<Position>(10, Position{0, 0});
 
     std::string line;
     while (std::getline(file, line))
     {
-        // assert(line.size() == 3);
         auto mod_x = 0;
         auto mod_y = 0;
         switch (line[0])
@@ -41,40 +75,12 @@ auto main() -> int
         auto count = std::stoi(line.substr(2));
         for (int i = 0; i < count; i++)
         {
-            head.x += mod_x;
-            head.y += mod_y;
-            if (abs(head.x - tail.x) >= 2)
-            {
-                tail.x = head.x - mod_x;
-                tail.y = head.y;
-            }
-            else if (abs(head.y - tail.y) >= 2)
-            {
-                tail.y = head.y - mod_y;
-                tail.x = head.x;
-            }
-            if (std::ranges::find(visited, tail) == visited.end())
-                visited.push_back(tail);
-            // for (int i = 0; i < 5; i++)
-            // {
-            //     for (int j = 0; j < 6; j++)
-            //     {
-            //         auto p = Position{j, i};
-            //         if (p == tail)
-            //             std::cout << "T";
-            //         else if (p == head)
-            //             std::cout << "H";
-            //         else if (std::ranges::find(visited, p) == visited.end())
-            //             std::cout << ".";
-            //         else
-            //             std::cout << "#";
-            //     }
-            //     std::cout << std::endl;
-            // }
-            //     std::cout << std::endl;
+            rope_step(rope1, mod_x, mod_y, visited1);
+            rope_step(rope2, mod_x, mod_y, visited2);
         }
     }
-    std::cout << "Part 1: " << visited.size() << std::endl;
+    std::cout << "Part 1: " << visited1.size() << std::endl;
+    std::cout << "Part 2: " << visited2.size() << std::endl;
 
     return 0;
 }
